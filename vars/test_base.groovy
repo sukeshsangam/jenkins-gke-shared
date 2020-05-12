41e24fa0b539e3e4ea052a204135e7d04bb69c9f
sukeshsangam <sukesh.sangam@gmail.com>
2020-05-12 07:26:48 -0400

    Create test_base.groovy

---
+++ vars/test_base.groovy
@@ -0,0 +1,16 @@
+podTemplate(label: 'kubernetes',
+  containers: [
+    containerTemplate(name: 'test-centos', image: 'centos:centos7', ttyEnabled: true, command: 'cat')
+  ]) {
+  stage('Preparation') {
+    node("kubernetes") {
+      container("test-centos") {
+        
+        git 'https://github.com/sukeshsangam/jenkins-gke-shared.git';
+        sh "echo hello"
+        //junit '**/target/surefire-reports/TEST-*.xml'
+        //archive 'target/*.jar'
+      }
+    }
+  }
+}

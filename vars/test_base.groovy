038087855fc8bdddf1d9d65ddbcca0d5509ddb8c
sukeshsangam <sukesh.sangam@gmail.com>
2020-05-12 07:48:27 -0400

    Update test_base.groovy

--- vars/test_base.groovy
+++ vars/test_base.groovy
@@ -1,9 +1,18 @@
+import utilities.*
+
 def call(body)
 {
+  def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    // creating an instance of the JenkinsSharedLibrary class
+    def instance = new JenkinsSharedLibrary(this)
+  
 podTemplate(label: 'kubernetes',
   containers: [
     containerTemplate(name: 'test-centos', image: 'centos:centos7', ttyEnabled: true, command: 'cat')
   ]) {
-  
+  instance.preparation()
 }
 }

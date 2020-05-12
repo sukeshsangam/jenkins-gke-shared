684c3e999b8c9716d48215f292c53a5364b6450f
sukeshsangam <sukesh.sangam@gmail.com>
2020-05-12 07:50:05 -0400

    Update JenkinsSharedLibrary.groovy

--- src/utilities/JenkinsSharedLibrary.groovy
+++ src/utilities/JenkinsSharedLibrary.groovy
@@ -44,46 +44,5 @@ class JenkinsSharedLibrary implements Serializable
       }
     }
   }
-      
-      
-      
-      }
-
-      def checkout(String git_url, String[] node_label)
-      {
-          steps.stage('checkout')
-          {
-              steps.echo "$node_label"
-              /*
-              iterating over the nodes and performing checkout of the function
-              */
-              for (int i = 0; i < node_label.size(); i++)
-              {
-                  steps.echo "####################  checkout in ${node_label[i]}  ######################"
-                  steps.node((String)node_label[i])
-                  {
-
-                      /*
-                      Geting report name and the branch name of the Job
-                      */
-                      def report = steps.sh( script: "echo ${steps.JOB_NAME} | awk -F '/' '{print \$1}'", returnStdout: true).trim()
-                      def branch = steps.sh( script: "echo ${steps.JOB_NAME} | awk -F '/' '{print \$2}'", returnStdout: true).trim()
-
-                      /*
-                      The below checkout function is the part of the steps class which is provided by default
-                      It does checkout by connecting to github using the provided CredentialID
-
-                      */
-                      steps.checkout([$class: 'GitSCM',
-                          branches: [[name: "*/$branch"]],
-                          doGenerateSubmoduleConfigurations: false,
-                          gitTool: 'Default',
-                          url: "$git_url"]]])
-
-                  }
-
-              }
-          }
-      }
-
+     
       }

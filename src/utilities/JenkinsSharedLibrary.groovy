41cf098818cdfbe2c404ae695227be81ad15dac1
sukeshsangam <sukesh.sangam@gmail.com>
2020-05-21 15:20:43 -0400

    Update JenkinsSharedLibrary.groovy

--- src/utilities/JenkinsSharedLibrary.groovy
+++ src/utilities/JenkinsSharedLibrary.groovy
@@ -41,7 +41,7 @@ class JenkinsSharedLibrary implements Serializable
        steps.echo "hello"
         //junit '**/target/surefire-reports/TEST-*.xml'
       
-        steps.archive('/*')
+        steps.archive('/*/*/*/*/*')
    
       }
     }

1e4c958f66aded732701d864b5a439b20c5178dd
sukeshsangam <sukesh.sangam@gmail.com>
2020-05-21 19:49:28 -0400

    Update JenkinsSharedLibrary.groovy

--- src/utilities/JenkinsSharedLibrary.groovy
+++ src/utilities/JenkinsSharedLibrary.groovy
@@ -42,7 +42,7 @@ class JenkinsSharedLibrary implements Serializable
             steps.echo '$PWD'
         //junit '**/target/surefire-reports/TEST-*.xml'
       
-        steps.archiveArtifacts ('/home/jenkins/agent/workspace/test-echo/**/*.*')
+        steps.archiveArtifacts ('**/*.*')
    
       }
     }

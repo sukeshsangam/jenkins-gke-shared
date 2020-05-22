ba1308c70acc3f8504df12ec2ed893913f60b9c8
sukeshsangam <sukesh.sangam@gmail.com>
2020-05-21 20:45:26 -0400

    Update JenkinsSharedLibrary.groovy

--- src/utilities/JenkinsSharedLibrary.groovy
+++ src/utilities/JenkinsSharedLibrary.groovy
@@ -58,7 +58,7 @@ class JenkinsSharedLibrary implements Serializable
           
          //  def customImage = steps.docker.build("my-image:78")
           
-          
+          steps.sh "yum update && yum -y install sudo"
           steps.sh "sudo yum check-update"
           steps.sh "curl -fsSL https://get.docker.com/ | sh"
           
